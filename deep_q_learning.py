adbaa676b453a972dc595ec79a3f40d093411d6b
jajokine <99346568+jajokine@users.noreply.github.com>
2022-02-17 20:13:49 +0200

    Update deep_q_learning.py

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -94,7 +94,7 @@ def deep_q_learning(current_state_vector, action_index, object_index, reward, ne
         None
     """
     
-    with torch.no_grad():
+    with torch.no_grad():                                                                                       # Disabling gradient calculation to save memory
         q_values_action_next, q_values_object_next = model(next_state_vector)
     maxq_next = 1/2 * (q_values_action_next.max() + q_values_object_next.max())
 
@@ -104,7 +104,7 @@ def deep_q_learning(current_state_vector, action_index, object_index, reward, ne
     maxQ = 0.0 if terminal else maxq_next                                                                       # Checking if episode is over
     y = reward + GAMMA * maxQ                                                                                   # y = R(s, c) + gamma[Max(Q)(s',c')
 
-    loss = 1/2 * (y - Q_val_current)**2
+    loss = 1/2 * (y - Q_val_current)**2                                                                         # L(theta) = 1/2(y-Q(s,c,theta))^2
 
     optimizer.zero_grad()
     loss.backward()
