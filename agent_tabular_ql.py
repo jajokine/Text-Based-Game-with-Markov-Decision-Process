3b2ddbe0425a52254268d59fd1e7e6227a54bc67
jajokine <99346568+jajokine@users.noreply.github.com>
2022-02-17 19:52:51 +0200

    Update agent_tabular_ql.py

--- agent_tabular_ql.py
+++ agent_tabular_ql.py
@@ -102,7 +102,7 @@ def run_episode(for_training):
     epsilon = TRAINING_EP if for_training else TESTING_EP
 
     
-    epi_reward = 0.0                                                                  # Initializing rewards
+    episode_reward = 0.0                                                              # Initializing rewards
     
 
     (current_room_desc, current_quest_desc, terminal) = framework.newGame()           # Descriptions of the current room and current quest state
@@ -140,12 +140,12 @@ def run_episode(for_training):
 
         if not for_training:
             
-            epi_reward += (GAMMA**(framework.STEP_COUNT - 1)) * reward                # Updating reward
+            episode_reward += (GAMMA**(framework.STEP_COUNT - 1)) * reward            # Updating reward
 
         current_room_desc, current_quest_desc = next_room_desc, next_quest_desc       # Preparing next step
 
     if not for_training:
-        return epi_reward
+        return episode_reward
 
 
 def run_epoch():
